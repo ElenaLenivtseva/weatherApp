a64216b18349a3a428c9b9c9c09c785b4a67e393
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-24 00:24:04 +0700

    Добавлен компонент Highlights, выполнен UI одного - скорости ветра

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import './App.css'
-import Temperature from './components/Temperature'
+import "./App.css";
+import Highlights from "./components/Highlights";
+import Temperature from "./components/Temperature";
 
 function App() {
- 
   return (
-   <div className='bg-[#1F213A] h-screen flex justify-center align-top'>
-    <div className='mt-40 w-1/5 h-1/3'><Temperature/></div>
-    <div className='bg-blue-400 mt-40 w-1/3 h-1/3'>right</div>
+    <div className="bg-[#1F213A] h-screen flex justify-center align-top">
 
-   </div>
-   
-  )
+      <div className="mt-40 w-1/5 h-1/3">
+        <Temperature />
+      </div>
+
+      <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6 ">
+        <h2 className="text-slate-200 text-2xl col-span-2">Today's Highlights</h2>
+        <Highlights />
+      </div>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
