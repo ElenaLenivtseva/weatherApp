30190e84d9fc36969ccd5ae57063d5953fca5b7d
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-24 13:53:15 +0700

    Правая сторона сделана динамической. Не уверена насчет разметки, насчет всего, потому что Vite хоть и зпускает сервер, выдает пустой экран, редко когда показывает, что на самом деле есть, т.е. я делаю вслепую. Про Tailwind CSS - мне не понравился абсолютно, потому что 1) повтор кода, когда стили повторяются, 2) нет привычных классов, сложно ориентироваться в разметке. По стилям, что ли, div-ы отличать? Не понимаю

--- src/App.jsx
+++ src/App.jsx
@@ -29,23 +29,49 @@ function App() {
   return (
     <div className="bg-[#1F213A] h-screen flex justify-center align-top">
       <div className="mt-40 w-1/5 h-1/3">
-        <Temperature setCity={setCity} stats={{
-          temp: weatherData.current.temp_c,
-          condition: weatherData.current.condition.text,
-          idDay: weatherData.current.is_day,
-          location: weatherData.location.name,
-          time: weatherData.location.localtime,
-          }}/>
+        {weatherData && (
+          <Temperature
+            setCity={setCity}
+            stats={{
+              temp: weatherData.current.temp_c,
+              condition: weatherData.current.condition.text,
+              idDay: weatherData.current.is_day,
+              location: weatherData.location.name,
+              time: weatherData.location.localtime,
+            }}
+          />
+        )}
       </div>
 
       <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6 ">
         <h2 className="text-slate-200 text-2xl col-span-2">
           Today's Highlights
         </h2>
-        <Highlights />
-        <Highlights />
-        <Highlights />
-        <Highlights />
+        {weatherData && (
+          <>
+            <Highlights stats={{
+              title: 'Wind Status',
+              value: weatherData.current.wind_mph,
+              unit: 'mph',
+              direction: weatherData.current.wind_dir
+            }}/>
+            <Highlights stats={{
+              title: 'Humidity',
+              value: weatherData.current.humidity,
+              unit: '%',
+            }}/>
+            <Highlights stats={{
+              title: 'Visibility',
+              value: weatherData.current.vis_miles,
+              unit: 'miles',
+            }}/>
+            <Highlights stats={{
+              title: 'Air Pressure',
+              value: weatherData.current.pressure_mb,
+              unit: 'mb',
+            }}/>
+          </>
+        )}
       </div>
     </div>
   );
